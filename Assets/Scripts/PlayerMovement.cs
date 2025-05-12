ca3740506903c30fe923818c36012eae768b50d4
Riccardo Ranazzi <ric.ranazzi@stud.uniroma3.it>
2025-05-12 16:24:53 +0200

    Server room

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        move.y = 0;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
+
+
